f80765c3876b07c86e5073abb6568322db53819d
21130407KhuongVo <21130407@st.hcmuaf.edu.vn>
2025-01-16 20:14:46 +0700

    update about_us page

--- pages/about_us/about.js
+++ pages/about_us/about.js
@@ -1,6 +1,6 @@
 async function setHeader() {
     $('#navigation').load('../../components/header.html', function () {
-        const selected = "nav_home"
+        const selected = "nav_about"
         setHeader(selected)
     })
 }
@@ -111,10 +111,6 @@ function setFacebook(data) {
     });
 }
 
-function loadRecommendPlaylist() {
-    $('#recommend_playlist').load('homepage/recommend_playlist.html')
-}
-
 function loadMembers() {
     $.getJSON("../../data/information.json", function (data) {
         // Gọi các hàm để hiển thị thông tin từ JSON
@@ -126,43 +122,4 @@ function loadMembers() {
         setLinkedln(data);
         setFacebook(data);
     });
-}
-
-function loadContentRecommendPlaylist() {
-    $.getJSON('information.json', function (data) {
-        // Lặp qua mảng medias
-        $.each(data.medias, function (index, media) {
-            // Tạo thẻ HTML cho mỗi bài hát
-            let mediaCard = `
-                <div class="col-lg-3 col-md-4 col-sm-6 mb-3 song-item" data-index="${media.id_media}">
-                <div class="card">
-                    <img src="images/Covers/${media.cover_image}" class="card-img-top" alt="${media.title}" loading="lazy">
-                    <div class="card-body d-flex flex-column">
-                        <a href="${media.spotify_url}" class="card-title link-underline link-underline-dark">${media.title}</a>
-                        <a href="#" class="card-text">${media.artist}</a>
-                    </div>
-                </div>
-            </div>
-        `;
-            // Thêm thẻ HTML vào phần tử chứa
-            $('#content_recommend_playlist').append(mediaCard);
-
-            // Add event listeners for song clicks
-            $('.song-item').click(function () {
-                const songIndex = $(this).data('index');
-
-                // Set song index in the URL
-                const newUrl = new URL(window.location.href);
-                newUrl.searchParams.set('songIndex', songIndex); // Add songIndex parameter to URL
-                window.history.pushState({}, '', newUrl); // Update the URL without reloading the page
-
-                // Play the selected song
-                setSong(songIndex - 1);
-            });
-        });
-    });
-}
-
-function loadYourLibrary() {
-    $('#your_library').load('homepage/your_library.html')
 }
